218174a715b4fe2c3f0310e0fb7994656fcf616e
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-09 15:40:37 +0200

    program 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace HelloWorld
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            int num1 = 5;
+            int numero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El número introducido es: "+valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
         }
     }
 }
